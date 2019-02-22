2459111bf42242180e9ad6fa97f404afb05a3a42
marcos busto <marcosbustos694@gmail.com>
2019-02-22 14:27:11 -0300

    Agregar archivos de proyecto.

---
+++ AlumnosCalificaciones/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlumnosCalificaciones.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
