7d67c08bb0df0336b6c5bb21792e4fa4c9a7071b
marcos busto <marcosbustos694@gmail.com>
2019-02-25 20:25:50 -0300

    creacion de tablas,contadores y ordenadores

---
+++ AlumnosCalificaciones/Models/Notas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlumnosCalificaciones.Models
+{
+    public class Notas
+    {
+        public int Id { set; get; }
+        public string Nombre { set; get; }
+        public int Nota { set; get; }
+    };
+}
--- AlumnosCalificaciones/wwwroot/js/site.js
+++ AlumnosCalificaciones/wwwroot/js/site.js
@@ -1,4 +1,114 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var vm = new Vue({
+    el: "#app",
+    data: {
+        contador1: 0,
+        contador2: 0,
+        contador3: 0,
+        contador4: 0,
+        contador5: 0,
+        contador6: 0,
+        contador7: 0,
+        contador8: 0,
+        contador9: 0,
+        contador10: 0,
 
-// Write your JavaScript code.
+       calificacion: {
+            nombre: '',
+            nota: 0,
+            
+        },
+        
+        Calificaciones: [],
+
+             
+    },
+    
+    
+
+    methods: {
+        agregarcalificacion: function () {
+
+
+            this.calificacion.nota = parseInt(this.calificacion.nota);
+
+            switch (this.calificacion.nota) {
+                case 1:
+                    this.contador1 = this.contador1 + 1;
+                    break;         
+            };
+            switch (this.calificacion.nota) {
+                case 2:
+                    this.contador2 = this.contador2 + 1;
+                    break;
+            };
+            switch (this.calificacion.nota) {
+                case 3:
+                    this.contador3 = this.contador3 + 1;
+                    break;
+            };
+            switch (this.calificacion.nota) {
+                case 4:
+                    this.contador4 = this.contador4 + 1;
+                    break;
+            };
+            switch (this.calificacion.nota) {
+                case 5:
+                    this.contador5 = this.contador5 + 1;
+                    break;
+            };
+            switch (this.calificacion.nota) {
+                case 6:
+                    this.contador6 = this.contador6 + 1;
+                    break;
+            };
+            switch (this.calificacion.nota) {
+                case 7:
+                    this.contador7 = this.contador7 + 1;
+                    break;
+            };
+            switch (this.calificacion.nota) {
+                case 8:
+                    this.contador8 = this.contador8 + 1;
+                    break;
+            };
+            switch (this.calificacion.nota) {
+                case 9:
+                    this.contador9 = this.contador9 + 1;
+                    break;
+            };
+            switch (this.calificacion.nota) {
+                case 10:
+                    this.contador10 = this.contador10 + 1;
+                    break;
+            };
+
+
+
+            this.Calificaciones.push(this.calificacion);
+
+            this.calificacion = {
+                nombre: '',
+                nota: 0,
+                
+            };
+        },
+        OrdenarListas: function (lista) {
+            lista = lista.sort(function (a, b) {
+                if (a.nombre > b.nombre) {
+                    return 1;
+                }
+                if (a.nombre < b.nombre) {
+                    return -1;
+                }
+
+                return 0;
+            });
+
+            return lista;
+        }
+                      
+    },
+
+    
+})
+
